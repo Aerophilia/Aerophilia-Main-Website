d1f296e533d028d1775761d956e9b2f0e71636dc
Al Ashqan Babu <80443704+alashqan@users.noreply.github.com>
2022-10-07 13:54:40 +0530

    Add files

---
+++ src/pages/home/brochure/Brochure.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Brochure.css'
+
+export default function Brochure() {
+  return (
+    <div className='brochure_div'>
+      <h3>Download Rulebook</h3>
+      <p>Details and shedule of Aerophilia 2022</p>
+      <button>Download</button>
+    </div>
+  )
+}
