55068ff440da164bf9a914e2371b82350190567b
Al Ashqan Babu <80443704+alashqan@users.noreply.github.com>
2022-10-07 11:27:13 +0530

    Add files

---
+++ src/components/copyright/Copyright.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Copyright.css'
+
+export default function Copyright() {
+  return (
+    <div>
+      <div className="copyright">
+        <p>Copyright © 2022 <strong>Aerophilia</strong>. All Rights Reserved. </p>
+        <p>Developed by <strong><a href="https://challengers.org.in/">TEAM CHALLENGERS</a></strong></p>
+      </div>
+    </div>
+  )
+}
