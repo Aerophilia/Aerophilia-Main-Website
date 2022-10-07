df6ab53c9669c0887a9761d746911eddd76a0643
Al Ashqan Babu <80443704+alashqan@users.noreply.github.com>
2022-10-07 13:59:29 +0530

    Add files

---
+++ src/pages/sponsor/Sponsor.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Sponsor() {
+  return (
+    <div className='sponsor_div'>
+      <h1 className='subtitle'>ABOUT <div className='bottom_line'></div>
+      </h1>
+    </div>
+  )
+}
